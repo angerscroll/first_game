680b7b0e678eca1897b0626c90cf0bda333059f4
angerscroll <angerscroll@gmail.com>
2023-02-18 13:52:46 +0900

    my_third_commit

--- js/index.js
+++ js/index.js
@@ -3,29 +3,50 @@ var dx = 3;
 var dy = -3;
 var x = 140;
 var y = 140;
-
+var paddle = document.getElementById("paddle");
 var board = document.getElementById("gameboard");
-board.addEventListener("keydown", moveBallHandler);
-board.focus();
-
-function moveBallHandler(event) {
-  if (event.keyCode === 37) {
-    // 왼쪽 화살표
-    x -= 10;
-  } else if (event.keyCode === 39) {
-    // 오른쪽 화살표
-    x += 10;
+var paddleWidth = paddle.offsetWidth;
+function movePaddle(event) {
+    const key = event.key;
+    if (key === "ArrowLeft") {
+      let paddleLeft = paddle.offsetLeft;
+      if (paddleLeft > 0) {
+        paddle.style.left = paddleLeft - 50 + "px";
+      }
+    } else if (key === "ArrowRight") {
+      let paddleLeft = paddle.offsetLeft;
+      if (paddleLeft < board.offsetWidth - paddleWidth) {
+        paddle.style.left = paddleLeft + 50 + "px";
+      }
+    }
   }
-  ball.style.left = x + "px";
-}
+  
+document.addEventListener("keydown", movePaddle);
+
 setInterval(moveBall, 15);
 
 function moveBall() {
     x += dx;
     y += dy;
 
-    if (x > 280 || x < 0) dx = -dx;
-    if (y > 280 || y < 0) dy = -dy;
+    if (x > 280 || x < 0) {
+        dx = -dx;
+        if (x < 0) {
+          x = 0;
+        } else {
+          x = 280;
+        }
+    }
+
+    if (y > 280 || y < 0) {
+        dy = -dy;
+        if (y < 0) {
+            y = 0;
+        } else {
+            y = 280;
+        }
+    }
+    
 
     ball.style.left = x + "px";
     ball.style.top = y + "px";
