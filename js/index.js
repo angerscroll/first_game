a268a23f19f0abdfdca4998a3143d31ff02f4fbc
angerscroll <angerscroll@gmail.com>
2023-02-18 12:43:09 +0900

    my_second_commit

--- js/index.js
+++ js/index.js
@@ -4,6 +4,20 @@ var dy = -3;
 var x = 140;
 var y = 140;
 
+var board = document.getElementById("gameboard");
+board.addEventListener("keydown", moveBallHandler);
+board.focus();
+
+function moveBallHandler(event) {
+  if (event.keyCode === 37) {
+    // 왼쪽 화살표
+    x -= 10;
+  } else if (event.keyCode === 39) {
+    // 오른쪽 화살표
+    x += 10;
+  }
+  ball.style.left = x + "px";
+}
 setInterval(moveBall, 15);
 
 function moveBall() {
